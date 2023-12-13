ec50bb3f6935ef5dd71a19767d4f688d44876df1
naoki <n.globe.us@gmail.com>
2023-12-14 00:10:32 +0900

    some fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,14 @@ AWS_SECRET_ACCESS_KEY=$4
 echo "INPUT_DIR: $INPUT_DIR"
 echo "ACCESS_KEY: $ACCESS_KEY"
 
+echo $PATH
+
 # Install dependencies
 npm install -g csv2geojson
 npm install -g klaw
+
+# node_modules が install されている場所を確認
+npm root -g
 
 node /bin/excel2geojson.js $INPUT_DIR
 ls -l $INPUT_DIR
