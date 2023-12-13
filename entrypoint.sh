94bab373b876c1e27e29d6d78b8002b60545817d
Naoki Ohashi <n.globe.us@gmail.com>
2023-12-13 16:50:08 +0900

    entrypoint.sh を更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ AWS_SECRET_ACCESS_KEY=$4
 echo "INPUT_DIR: $INPUT_DIR"
 echo "ACCESS_KEY: $ACCESS_KEY"
 
-# node /bin/excel2geojson.js $INPUT_DIR
-# ls -l $INPUT_DIR
+node /bin/excel2geojson.js $INPUT_DIR
+ls -l $INPUT_DIR
 
 exit 0
