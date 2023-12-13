14ba0cf19f9117b86ee43a601357c8bc3e3b2d28
naoki <n.globe.us@gmail.com>
2023-12-14 08:38:59 +0900

    some fix

--- bin/excel2geojson.js
+++ bin/excel2geojson.js
@@ -12,23 +12,28 @@ const excelToGeoJson = async (inputDir) => {
   for await (const file of klaw(inputDir, { depthLimit: -1 })) {
     let csvData;
 
-    if (file.path.endsWith(".xlsx")) {
-      const excelPath = file.path;
-      try {
-        csvData = await excel2csv(excelPath);
-      } catch (err) {
+    // if (file.path.endsWith(".xlsx")) {
+    //   const excelPath = file.path;
 
-        if (err.message === "FILE_ENDED") {
-          throw new ConversionError("fileEnded", excelPath);
-        }
 
-        console.log(err);
+
+
+    //   try {
+    //     csvData = await excel2csv(excelPath);
+    //   } catch (err) {
+
+    //     if (err.message === "FILE_ENDED") {
+    //       throw new ConversionError("fileEnded", excelPath);
+    //     }
+
+    //     console.log(err);
         
-        throw new ConversionError("excelToGeoJson", excelPath);
-      }
-    } else if (file.path.endsWith(".csv")) {
+    //     throw new ConversionError("excelToGeoJson", excelPath);
+    //   }
+    // } else if (file.path.endsWith(".csv")) {
+      console.log(file.path)
       csvData = await readFile(file.path, 'utf-8');
-    }
+    // }
 
     if (csvData) {
       const geoJsonPath = file.path.replace(/.csv$|.xlsx$/, '.json');
