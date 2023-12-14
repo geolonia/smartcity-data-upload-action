f8f0ed77ba06abbc681b2373d51cce91f4442e15
naoki <n.globe.us@gmail.com>
2023-12-14 09:36:26 +0900

    some fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,8 @@ AWS_SECRET_ACCESS_KEY=$4
 npm install
 
 node ./bin/excel2geojson.js $INPUT_DIR
-# node ./bin/geojson2mbtiles.js $INPUT_DIR
+node ./bin/geojson2mbtiles.js $INPUT_DIR
 
 ls $INPUT_DIR
-# geojson を 表示
-cat ./docs/aed-locations-csv.geojson | jq
-cat ./docs/aed-locations-xlsx.geojson | jq
 
 exit 0
