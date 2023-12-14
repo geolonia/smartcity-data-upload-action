1ff56a00e24f1846181434288bc9bd0baf93fb88
naoki <n.globe.us@gmail.com>
2023-12-14 14:30:40 +0900

    認証情報を環境変数として追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
-set -e
+set -ex
 
 npm install
-
-ls
-
-echo "INPUT_DIR: $INPUT_DIR"
-echo "CITY_ID: $CITY_ID"
 
 node ./bin/excel2geojson.js $INPUT_DIR
 node ./bin/geojson2mbtiles.js $INPUT_DIR $CITY_ID
 node ./bin/createTilesJson.js ./$CITY_ID-v1.mbtiles
 
 pmtiles convert ./$CITY_ID-v1.mbtiles ./$CITY_ID-v1.pmtiles
-
-mkdir build
-mv ./$CITY_ID-v1.mbtiles ./build/$CITY_ID-v1.mbtiles
-
-ls $INPUT_DIR
-ls
-
-pwd
+pmtiles upload ./$CITY_ID-v1.pmtiles $CITY_ID-v1.pmtiles --bucket=s3://smartcity-data-upload-action-dev
 
 exit 0
