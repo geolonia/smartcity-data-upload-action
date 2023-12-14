99c96278ebbd278c1444993aa4582ba02fe2af21
naoki <n.globe.us@gmail.com>
2023-12-14 10:45:56 +0900

    テストコマンドを追加

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # smartcity-data-upload-action
+
+
+
+# ローカルで実行する
+
+```
+$ docker build -t geolonia/smartcity-data-upload-action .
+$ docker run --env-file ./.env.test geolonia/smartcity-data-upload-action
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 set -e
 
-INPUT_DIR=$1
-ACCESS_KEY=$2
-AWS_ACCESS_KEY=$3
-AWS_SECRET_ACCESS_KEY=$4
-
-
 npm install
-
-node ./bin/excel2geojson.js $INPUT_DIR
-node ./bin/geojson2mbtiles.js $INPUT_DIR
-
-ls $INPUT_DIR
 ls
+
+node ./bin/excel2geojson.js $input_dir
+node ./bin/geojson2mbtiles.js $input_dir
+
+ls $input_dir
+ls
+
 
 exit 0
